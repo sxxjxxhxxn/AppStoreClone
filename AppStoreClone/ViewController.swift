1ea847e50dfa39082dc3751c9951c591fc125c7b
sxxjxxhxxn <jaehoon572@naver.com>
2020-08-10 13:57:40 +0900

    Initial commit

---
+++ AppStoreClone/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AppStoreClone
+//
+//  Created by Paul.S on 2020/08/10.
+//  Copyright © 2020 Paul.S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}

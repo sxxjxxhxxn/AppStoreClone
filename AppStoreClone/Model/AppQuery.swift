0fb9da62c42d5c1a66c447a64115c17c270bae8c
sxxjxxhxxn <jaehoon572@naver.com>
2020-08-13 17:56:10 +0900

    AppQuery 추가

---
+++ AppStoreClone/Model/AppQuery.swift
@@ -0,0 +1,13 @@
+//
+//  AppQuery.swift
+//  AppStoreClone
+//
+//  Created by Paul.S on 2020/08/13.
+//  Copyright © 2020 Paul.S. All rights reserved.
+//
+
+import Foundation
+
+struct AppQuery: Equatable {
+    let query: String
+}

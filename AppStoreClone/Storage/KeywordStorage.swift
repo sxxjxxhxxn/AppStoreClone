ebbbbc774092837fd65a3fd94140946f076ea789
sxxjxxhxxn <jaehoon572@naver.com>
2020-09-02 13:00:13 +0900

    Storage 추가

---
+++ AppStoreClone/Storage/KeywordStorage.swift
@@ -0,0 +1,45 @@
+//
+//  KeywordStorage.swift
+//  AppStoreClone
+//
+//  Created by Paul.S on 2020/09/02.
+//  Copyright © 2020 Paul.S. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol KeywordStorageType {
+    func fetchKeywords() -> Observable<[Keyword]>
+    func saveKeyword(keyword: Keyword)
+}
+
+final class KeywordStorage: KeywordStorageType {
+    
+    private let maxStorageLimit: Int
+    private let storage: Storage
+
+    init(maxStorageLimit: Int, storage: Storage) {
+        self.maxStorageLimit = maxStorageLimit
+        self.storage = storage
+    }
+    
+    func fetchKeywords() -> Observable<[Keyword]> {
+        return BehaviorSubject.init(value: storage.fetch()).asObservable()
+    }
+    
+    func saveKeyword(keyword: Keyword) {
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let self = self else { return }
+            var keywords = self.storage.fetch().filter { $0 != keyword }
+            keywords.insert(keyword, at: 0)
+            self.storage.save(keywords: keywords, self.maxStorageLimit)
+        }
+    }
+}
+
+final class StorageProvider {
+    func makeKeywordStorage(maxStorageLimit: Int) -> KeywordStorageType {
+        return KeywordStorage(maxStorageLimit: maxStorageLimit, storage: Storage())
+    }
+}
---
+++ AppStoreClone/Storage/Storage.swift
@@ -0,0 +1,40 @@
+//
+//  Storage.swift
+//  AppStoreClone
+//
+//  Created by Paul.S on 2020/09/02.
+//  Copyright © 2020 Paul.S. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+final class Storage {
+    
+    private let recentKeywordsKey = "recentKeywords"
+    private var userDefaults: UserDefaults
+    
+    init(userDefaults: UserDefaults = UserDefaults.standard) {
+        self.userDefaults = userDefaults
+    }
+
+    func fetch() -> [Keyword] {
+        if let keywordsData = userDefaults.object(forKey: recentKeywordsKey) as? Data {
+            if let keywords = try? JSONDecoder().decode([Keyword].self, from: keywordsData) {
+                return keywords
+            }
+        }
+        return []
+    }
+
+    func save(keywords: [Keyword], _ maxStorageLimit: Int) {
+        let recentKeywords = removeOldKeywords(keywords, maxStorageLimit)
+        if let encoded = try? JSONEncoder().encode(recentKeywords) {
+            userDefaults.set(encoded, forKey: recentKeywordsKey)
+        }
+    }
+
+    private func removeOldKeywords(_ keywords: [Keyword], _ maxStorageLimit: Int) -> [Keyword] {
+        return keywords.count <= maxStorageLimit ? keywords : Array(keywords[0..<maxStorageLimit])
+    }
+}

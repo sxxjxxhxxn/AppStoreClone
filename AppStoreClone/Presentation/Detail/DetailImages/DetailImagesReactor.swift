b5243daec2ddafe1cfc68ea2442f533799e3d23b
sxxjxxhxxn <jaehoon572@naver.com>
2020-09-10 15:43:59 +0900

    DetailImagesVC, DetailImagesReactor 추가

---
+++ AppStoreClone/Presentation/Detail/DetailImages/DetailImagesReactor.swift
@@ -0,0 +1,26 @@
+//
+//  DetailImagesReactor.swift
+//  AppStoreClone
+//
+//  Created by Paul.S on 2020/09/10.
+//  Copyright © 2020 Paul.S. All rights reserved.
+//
+
+import Foundation
+import ReactorKit
+
+final class DetailImagesReactor: Reactor {
+    var initialState: State
+    
+    typealias Action = NoAction
+    
+    struct State {
+        let indexPath: IndexPath
+        let screenshotUrls: [String]
+    }
+    
+    init(indexPath: IndexPath, screenshotUrls: [String]) {
+        initialState = State(indexPath: indexPath, screenshotUrls: screenshotUrls)
+    }
+
+}
---
+++ AppStoreClone/Presentation/Detail/DetailImages/DetailImagesViewController.swift
@@ -0,0 +1,69 @@
+//
+//  DetailImagesViewController.swift
+//  AppStoreClone
+//
+//  Created by Paul.S on 2020/09/10.
+//  Copyright © 2020 Paul.S. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import ReactorKit
+
+final class DetailImagesViewController: UIViewController, View {
+    
+    typealias Reactor = DetailImagesReactor
+    var disposeBag = DisposeBag()
+    private let collectionView = UICollectionView(frame: CGRect.zero,
+                                                  collectionViewLayout: UICollectionViewFlowLayout()).then {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = UICollectionView.ScrollDirection.horizontal
+        layout.itemSize = CGSize(width: 294, height: 522)
+
+        $0.setCollectionViewLayout(layout, animated: true)
+        $0.register(ThumbnailCollectionViewCell.self, forCellWithReuseIdentifier: ThumbnailCollectionViewCell.reuseID)
+        $0.backgroundColor = UIColor.clear
+    }
+    private var completeButton: UIBarButtonItem?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUp()
+    }
+    
+    private func setUp() {
+        view.backgroundColor = UIColor.white
+        view.addSubview(collectionView)
+        completeButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(tapDone(sender:)))
+        navigationItem.rightBarButtonItem = completeButton
+        
+        collectionView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+    }
+
+    func bind(reactor: Reactor) {
+        reactor.state
+            .map { $0.screenshotUrls }
+            .bind(to: collectionView.rx.items) { (collectionView, _, imageUrl) -> UICollectionViewCell in
+                let cell = collectionView.dequeueReusableCell(of: ThumbnailCollectionViewCell.self)
+                cell.bind(imageUrl)
+                return cell
+            }
+            .disposed(by: disposeBag)
+        
+        collectionView.rx
+            .willDisplayCell
+            .take(1)
+            .do(onNext: { _ in
+                self.collectionView.scrollToItem(at: reactor.initialState.indexPath, at: .centeredHorizontally, animated: false)
+            })
+            .subscribe()
+            .disposed(by: disposeBag)
+    }
+    
+    @objc func tapDone(sender: UIBarButtonItem) {
+        self.dismiss(animated: true)
+    }
+    
+}
